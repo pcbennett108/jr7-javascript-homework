af0e22fdacdf81f54462754e56a2e52f3fc64619
pcbennett108 <pcbennett42@gmail.com>
2022-01-13 16:57:16 -0700

    added petlovers part 2

--- petLovers.js
+++ petLovers.js
@@ -27,3 +27,27 @@ In the function that is printing out, add an if statement.
 Make sure the code is re-usable for both adding the names, iterating through the array, 
     and printing the outputs. Avoid duplicate code. */
 //
+let zoo = [];
+function zooKeeper(critter) {
+  let tooLong = "Mommy, that animal name is too long to pronounce :(";
+  if (critter == null) {
+    console.log("No new critters were added.");
+  } else if (critter.length > 10) {
+    zoo.push(tooLong);
+  } else {
+    zoo.push(critter);
+  }
+  for (i = 0; i < zoo.length; i++) {
+    console.log(zoo[i]);
+  }
+}
+zooKeeper("Tiger");
+zooKeeper("Wunderpus photogenicus");
+zooKeeper("Bear");
+zooKeeper("Pink Fairy Armadillo");
+zooKeeper("Coyote");
+zooKeeper("Rasberry Crazy Ant");
+zooKeeper("Moose");
+zooKeeper("Satanic Leaf-Tailed Gecko");
+zooKeeper("Lion");
+zooKeeper();
